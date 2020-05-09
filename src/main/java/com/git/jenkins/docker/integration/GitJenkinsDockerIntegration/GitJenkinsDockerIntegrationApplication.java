5aee09ffab9620cc1e8334c51a861aaf6a442f6e
Bodapati Raghu Ram <raghurambodapati007@gmail.com>
2020-05-09 18:44:45 +0530

    First Commit

---
+++ src/main/java/com/git/jenkins/docker/integration/GitJenkinsDockerIntegration/GitJenkinsDockerIntegrationApplication.java
@@ -0,0 +1,15 @@
+package com.git.jenkins.docker.integration.GitJenkinsDockerIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJenkinsDockerIntegrationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GitJenkinsDockerIntegrationApplication.class, args);
+
+	}
+
+}
